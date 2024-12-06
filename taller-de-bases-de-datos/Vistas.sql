004871d7ffcd6604b7d6f1bad969e5c76fa3c558
lcoba <lcoba@outlook.com>
2024-12-06 02:05:34 -0500

    Vistas.sql added 12/6/2024

---
+++ taller-de-bases-de-datos/Vistas.sql
@@ -0,0 +1,18 @@
+-- Crea o modifica la vista Person.vwNombresDirecciones 
+-- Basado en el query join que esta despues del AS 
+CREATE OR ALTER VIEW Person.vwNombresDirecciones AS
+SELECT PP.BusinessEntityID, PP.FirstName, PP.LastName, PA.AddressLine1, PA.City
+	FROM Person.Person PP 
+	INNER JOIN Person.BusinessEntityAddress PB ON PP.BusinessEntityID = PB.BusinessEntityID
+	INNER JOIN Person.Address PA ON PB.AddressID = PA.AddressID
+		
+-- Una vez ejecutado el codigo anterio que crea la vista Person.vwNombresDirecciones
+-- Podemos hacer select a la vista como si fuera una tabla
+SELECT * FROM Person.vwNombresDirecciones
+
+
+
+
+
+
+
