1e19fb7d0999397a6d893c19a0c51fa37fd8d96b
Luis Coba Colli <lcoba@outlook.com>
2024-12-08 09:42:53 -0500

    Commit Files Funciones 12/8/2024

--- taller-de-bases-de-datos/Funciones.sql
+++ taller-de-bases-de-datos/Funciones.sql
@@ -1,2 +1,20 @@
--- Aqui van las funciones
--- Poner mas funciones que nos van
+-- Comando para usar la base de datos AdventureWorks2022
+-- Ejecuta este codigo primero
+-- Recibiras este mensaje Commands completed successfully.
+USE AdventureWorks2022
+
+-- Una funcion es un "objeto" en la base de datos que nos retorna un valor despues de 
+-- ejecutarla, este a su vez recibe parametros.
+CREATE OR ALTER Function Person.ComoSeLLama (@idEmpleado int)
+returns nvarchar(100) AS
+BEGIN 
+	DECLARE @nombre_ nvarchar(100);
+	
+	SELECT @nombre_ = CONCAT('Se llama: ', FirstName,'  ', LastName) FROM Person.Person WHERE BusinessEntityID = @IdEmpleado;
+	return @nombre_;
+END
+
+
+
+-- Para usar la funcion debemos de usar de la siguiente forma 
+SELECT Person.ComoSeLLama (234)
